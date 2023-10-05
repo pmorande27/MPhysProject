e1a3446c56e6f492e37413b3d269d22b36626a13
Pmorande27 <pmorande27@gmail.com>
2023-10-05 22:49:16 +0100

    lint

--- src/QSHOHMC.py
+++ src/QSHOHMC.py
@@ -48,19 +48,28 @@ class Lattice(object):
         self.DH = np.zeros(N_measurement)
         
         self.d_tau = d_tau
+        
         self.accepted = 0
+        
         self.tries = 0
+        
         if N_thermal!= 0:
+        
             print('Termalisation with a = ' + str(self.a) + " N = " +str(self.N)+ " and Accel = " + str(self.acceleration))
+        
             self.thermalize()
+        
             print('----------------------------------')
+        
         self.accepted = 0
+        
         self.tries = 0
         
     def A_k(self,k, M):
         """
         Computes the inverse of the Kernel in Fourier Space
         """
+        
         return self.a/(self.m*(4*np.sin(np.pi*k/self.N)**2+M**2))
     
     def HMC_accel(self):
@@ -68,17 +77,24 @@ class Lattice(object):
         Hybrid Monte Carlo with Fourier Acceleration
         """
 
+        
         N_2 = int(self.N/2)
+        
         k = np.arange(0, self.N) # lattice in Fourier space
+        
         A_k = (self.m/self.a * (4*np.sin(np.pi * k/self.N)**2 + (self.a*self.w)**2) )**(-1)
+        
         self.A = A_k
+        
         PI_std = np.sqrt(self.N /  A_k) 
+        
         Pi = np.random.normal(loc=0, scale=PI_std)
 
         ps_bar = np.zeros(self.N,dtype=complex)
+        
         ps_bar[0] = Pi[0]
+        
         ps_bar[N_2] = Pi[N_2]
-
 
         # unpack remaining PI into Fourier space pi
 
@@ -90,23 +106,24 @@ class Lattice(object):
         # pi is real by construction
         phi = self.lattice.copy()
 
-
         ps = np.real(np.fft.ifft(ps_bar))
-        #ps = np.ones(100)
-        #phi = np.ones(100)
-
+        
         H = 1/(2*self.N) *sum(np.abs(np.fft.fft(ps))**2*A_k) +Lattice.action(phi,self.a,self.m,self.w)
+        
         ps_f, phi_f = self.molecular_dynamics_accelerated(phi.copy(),ps.copy(),A_k)
-
 
         H_new = 1/(2*self.N) *sum(np.abs(np.fft.fft(-ps_f))**2*A_k) +Lattice.action(phi_f,self.a,self.m,self.w)
 
         delta_H = H_new- H
+
         self.delta_H = delta_H
 
         if delta_H <0 or np.exp(-delta_H) > np.random.uniform(0,1):
+
             self.accepted += 1
+
             self.lattice = phi_f.copy()
+
         self.tries += 1
 
     def molecular_dynamics_accelerated(self,phi_0,ps_0,A_k):
@@ -115,18 +132,21 @@ class Lattice(object):
         """
 
         p = ps_0 -self.d_tau/2 * (self.m/self.a *(2*phi_0-np.roll(phi_0,1)-np.roll(phi_0,-1))+ self.a*self.m*self.w**2 *phi_0 )
-        #print(p)
+
         p_f = np.fft.fft(p)
+        
         phi =phi_0 +np.real(np.fft.ifft(p_f*A_k))*self.d_tau
-        #print(phi)
 
         for i in range(0,self.N_tau):
 
             p = p -self.d_tau* (self.m/self.a *(2*phi-np.roll(phi,1)-np.roll(phi,-1))+ self.a*self.m*self.w**2 *phi )
+            
             p_f = np.fft.fft(p)
+            
             phi =phi +np.real(np.fft.ifft(np.multiply(p_f,A_k)) )*self.d_tau
 
         p = p -self.d_tau/2* (self.m/self.a *(2*phi-np.roll(phi,1)-np.roll(phi,-1))+ self.a*self.m*self.w**2 *phi )
+        
         return p,phi
     
 
@@ -157,7 +177,9 @@ class Lattice(object):
         self.delta_H = H_new- H
 
         if self.delta_H <0 or np.exp(-self.delta_H) > np.random.uniform(0,1):
+       
             self.accepted += 1
+        
             self.lattice = x_f.copy()
         self.tries += 1
     
@@ -167,11 +189,13 @@ class Lattice(object):
         Molecular Dynamics for the case of no acceleration
         """
         p = p_0 -self.d_tau/2 * (self.m/self.a *(2*x_0-np.roll(x_0,1)-np.roll(x_0,-1))+ self.a*self.m*self.w**2 *x_0 )
+       
         x = x_0 + self.d_tau*p
 
         for j in range(1,self.N_tau):
 
             p = p -self.d_tau * (self.m/self.a *(2*x-np.roll(x,1)-np.roll(x,-1))+ self.a*self.m*self.w**2 *x )
+        
             x = x + self.d_tau*p
 
         p = p -self.d_tau/2 * (self.m/self.a *(2*x-np.roll(x,1)-np.roll(x,-1))+ self.a*self.m*self.w**2 *x )
@@ -184,8 +208,11 @@ class Lattice(object):
         Runs the HMC alogrithm for N_thermal times
         """
         with alive_bar(self.N_thermal) as bar:
+        
             for i in range(self.N_thermal):
+        
                 if self.acceleration:
+       
                     self.HMC_accel()
 
                 else:
@@ -196,21 +223,33 @@ class Lattice(object):
         Runs the HMC for Calibration of the parameters of the algorithms
         """
         with alive_bar(N_runs+N_thermal) as bar:
+        
             for i in range(N_thermal):
+        
                 if self.acceleration:
+        
                     self.HMC_accel()
+         
                 else:
+           
                     self.HMC()
+           
                 bar()
+           
             self.accepted = 0
+           
             self.tries = 0
+            
             for i in range(N_runs):
 
                 if self.acceleration:
+            
                     self.HMC_accel()
+            
                 else:
     
                     self.HMC()
+            
                 bar()
     
 
@@ -221,9 +260,11 @@ class Lattice(object):
         """
     
         results = np.empty((self.N_measurement,self.N))
+        
+        print('Measurements with a = ' + str(self.a) + " N = " +str(self.N)+ " and Accel = " + str(self.acceleration))
+ 
         with alive_bar(self.N_measurement) as bar:
 
-    
             for i in range(self.N_measurement):
         
                 for j in range(self.N_sweeps):
@@ -247,7 +288,9 @@ class Lattice(object):
         """
 
         results = [0 for i in range(self.N_measurement)]
+        
         print('Measurements with a = ' + str(self.a) + " N = " +str(self.N)+ " and Accel = " + str(self.acceleration))
+        
         with alive_bar(self.N_measurement) as bar:
 
 
@@ -256,17 +299,24 @@ class Lattice(object):
                 for j in range(self.N_sweeps):
 
                     if self.acceleration:
+                     
                         self.HMC_accel()
 
                     else:
                         self.HMC()
 
                 if i% 1000 == 0:
+                    
                     pass
+                
                 self.DH[i]  = self.delta_H 
+                
                 results[i] = observable(self.lattice)
+               
                 bar()
+        
         print('------------------------------')
+        
         return results
 
     @staticmethod
@@ -277,9 +327,11 @@ class Lattice(object):
         N = len(lattice)
 
         positions = [i for i in range(N)]
+        
         values = [0 for i in range(positions)]
       
         for i in range(len(positions)):
+           
             values[i] = lattice[0]*lattice[positions[i]]
 
         return values
@@ -296,6 +348,7 @@ class Lattice(object):
 
         
         for n in range(N+1):
+            
             values[n] = 1/N* sum( np.roll(lattice,-n)*lattice)
         
 
@@ -306,12 +359,14 @@ class Lattice(object):
         """
         Returns the average position of a given lattice
         """
+        
         return np.average(lattice)
     @staticmethod
     def measure_sq_position(lattice):
         """
         Returns the average sq position of a given lattice
         """
+        
         return np.average([x**2 for x in lattice])
     
     @staticmethod
@@ -319,6 +374,7 @@ class Lattice(object):
         """
         Option to save files
         """
+        
         file = open(file_name,'a')
         
         file.write("########################"+ '\n')
