44708ed9c8fd41701fca786f478afac52156b4f4
Pmorande27 <pmorande27@gmail.com>
2023-10-18 14:12:53 +0100

    Add structure of program for Chiral Model

--- src/Chiral.py
+++ src/Chiral.py
@@ -1,83 +1,56 @@
 import numpy as np
-import SUroutines as SU2
+from alive_progress import alive_bar
+
 
 class Chiral(object):
-    def __init__(self,N, beta, D = 2) -> None:
-
+    def __init__(self,N, beta, N_measurment, N_thermal, N_sweeps, SU=3, a = 1) -> None:
+        self.SU = 3
+        self.N_sweeps = N_sweeps
+        if SU == 3 or SU == 2:
+            self.c = 2
+        else:
+            raise ValueError('Not considered value for SU')
         self.N = N
-
-        self.U = np.zeros((N,N,4),dtype=float) 
+        self.N_measurement = N_measurment
+        self.N_thermal = N_thermal
+        self.U = np.zeros((N,N,2,2),dtype=float) 
         self.beta = beta
-        a = np.arange(N*N).reshape((N,N))
-
-        for i in range(N):
+        self.generators = self.create_generators()
+        self.DH = np.zeros(self.N_measurement)
+        #print(generators)
+        """for i in range(N):
 
             for j in range(N):
-                    self.U[i,j] = [1,0,1,0]
-
-        self.NN_mask = self.make_NN_mask()
-    def make_NN_mask(self):
-        '''Makes mask to apply to phi or pi which then gives the matrix parameter values of the nearest neighbors (NN) for each lattice site.
-        Hence phi[self.NN_mask] is of shape (N,N,#neighbors,#parameters) i.e (N,N,4,4).
+                    self.U[i,j] = [1,0,1,0]"""
         
-        Returns:
-        NN_mask: tuple
-            tuple of two (N,N,4,1) arrays, each giving the row and column coordinate for all nearest neighbors
-        '''
-     
-        # make a (N,N,2) array storing the row and col indices of each lattice sites
-        grid = np.indices((self.N,self.N)) 
-        lattice_coords = grid.transpose(1,2,0)
+        for i in range(N):
+            for j in range(N):
+                for k in range(2):
+                    self.U[i,j,k,k]  = 1
+        self.thermalize()
 
-        # shift lattice coordinates by 1 such that the coordinates at (i,j) are those of the right, left, top, and bottom neighbor of lattice site (i,j)
-        # rolling axis=1 by -1 means all columns are moved one step to the left with periodic bcs. Hence value of resulting array at (i,j) is (i,j+1), i.e the coordinates of the right neighbor.
-        # all of shape (N,N,2)
-        right_n = np.roll(lattice_coords, -1, axis=1)
-        left_n = np.roll(lattice_coords, 1, axis=1)
-        top_n = np.roll(lattice_coords, 1, axis=0)
-        bottom_n = np.roll(lattice_coords, -1, axis=0)
+    def create_generators(self):
+        generators = np.zeros((8,3,3),dtype=complex)
+        generators[0][0,1] = 1
+        generators[0][1,0] = 1
+        generators[1][0,1] = -1j
+        generators[1][1,0] = 1j
+        generators[2][0,0]  = 1
+        generators[2][1,1] = -1
+        generators[3][0,2] = 1
+        generators[3][2,0] = 1
+        generators[4][0,2] = -1j
+        generators[4][2,0] = 1j
+        generators[5][1,2] = 1
+        generators[5][2,1] = 1
+        generators[6][1,2] = -1j
+        generators[6][2,1] = 1j
+        generators[7][0,0] = 1/np.sqrt(3)
+        generators[7][1,1] = 1/np.sqrt(3)
+        generators[7][2,2] = -2/np.sqrt(3)
+        return generators
 
-        # for each lattice site, for each neighbor, store row and column coordinates
-        # order of neighbors: right, left, top, bottom
-        NN = np.empty((self.N,self.N,4,2), dtype=int)
-        NN[:,:,0,:] = right_n # row and col indices of right neighbors
-        NN[:,:,1,:] = left_n
-        NN[:,:,2,:] = top_n
-        NN[:,:,3,:] = bottom_n
-
-        # make mask to index phi
-        # separate the row and column neighbor coordinates for each lattice site: (N,N,4,1)
-        NN_rows = NN[:,:,:,0]
-        NN_cols = NN[:,:,:,1]
-        NN_mask = (NN_rows, NN_cols)
-
-        return NN_mask 
-       
-    def action_2(self, phi):
-        '''
-        Computes the action for lattice configuration phi
-        phi: (N,N,4) array
-            parameter values of SU(2) matrices at each lattice site
-
-        Returns
-        S: float
-            the action
-        '''
-        #phi_hc = np.matrix(phi).getH()
-        phi_NN = phi[self.NN_mask] # (N,N,4,4): containing the 4 paras of each of the 4 NN
-        phi_hc = SU2.hc(phi)
-        # sum over lattice unit vectors: to the right and up. Hence only need right and top NN, stored at position 0,3 respectively
-        G = np.zeros((self.N,self.N))
-        print(phi)
-        for i in [0,3]:
-        
-            A = SU2.dot(phi_hc, phi_NN[:,:,i,:])
-            G += SU2.tr(A + SU2.hc(A)) # when getting UFuncTypeError, check that dtype of G and SU2.tr is the same (float64 by default)
-
-        # sum over lattice sites    
-        S = -1/2 * self.beta * np.sum(G)
-
-        return S
+    
 
     @staticmethod
     def action(U, beta):
@@ -89,7 +62,124 @@ class Chiral(object):
                 G+= np.dot(np.matrix(U[t,x]).getH(),top_neighbours[t,x])  +np.matrix(np.dot(np.matrix(U[t,x]).getH(),top_neighbours[t,x])).getH()
                 G+= np.dot(np.matrix(U[t,x]).getH(),right_neighbours[t,x])  + np.matrix(np.dot(np.matrix(U[t,x]).getH(),right_neighbours[t,x])).getH()
         return -beta/2* np.trace(G)
-a = Chiral(2,2)
+    @staticmethod
+    def Hamiltonian(p,U,beta, c):
+        return 1/(2*c)*np.trace(np.matmul(p[:,:],p[:,:]),axis1=2,axis2=3) + Chiral.action(U,beta)
+    @staticmethod
+    def exponential_matrix(matrix, order):
+        return np.sum([np.linalg.matrix_power(matrix,n)/np.math.factorial((n)) for n in range(order)],axis = 0,dtype = matrix.dtype)
+    
+    def dot_p(U, beta):
+        G = np.zeros((len(U),len(U),3,3),dtype=complex)
+        top_neighbours = np.roll(U,-1,axis = 0)
+        bottom_neighbours = np.roll(U,1,axis = 0)
+        left_nighbours = np.roll(U,-1,axis = 1)
+        right_neighbours = np.roll(U,1,axis = 1)
+        for t in range(len(U)):
+            for x in range(len(U)):
+                upper_a = np.dot(top_neighbours[t,x] + bottom_neighbours[t,x],np.matrix(U[t,x]).getH())
+                sider_a =  np.dot(left_nighbours[t,x] + right_neighbours[t,x],np.matrix(U[t,x]).getH())
+                G[t,x]= upper_a + np.matrix(upper_a).getH() + sider_a + np.matrix(sider_a).getH() 
+        
+        return -1j-beta*G
+    
+    def molecular_dynamics(p_0, U_0, beta, epsilon, N_tau):
+        p = 0
+        U = 0
+        for i in range(N_tau):
+            p = 0
+            U = 0
+        p = 0
+        return p, U
+    
+    def HMC(self):
+        p_i = np.random.normal(loc=0,scale=1,size=(self.N,self.N, 8))
+        p_i = np.random.normal(loc=0,scale=1,size=(N,N, 8))
+        p = np.zeros((self.N,self.N,self.SU,self.SU),dtype=complex)
+        for i in range(self.N):
+            for j in range(self.N):
+                p[i,j] = sum(p_i[i,j,k] * self.generators[k] for k in range(int(self.SU**2-1)))
+        
+        H = Chiral.Hamiltonian(p,self.U,self.beta,self.c)
+        p_new, U_new = Chiral.molecular_dynamics(p.copy(),self.U.copy(),self.beta,self.epsilon,self.N_tau)
+        H_new = Chiral.Hamiltonian(p_new,U_new, self.beta, self.c)
+        Delta_H = H_new - H
+        self.delta_H = Delta_H
+        if Delta_H <0 or np.exp(-Delta_H) > np.random.uniform(0,1):
+       
+            self.accepted += 1
+        
+            self.U = U_new.copy()
+        self.tries += 1
+
+    def thermalize(self):
+        """
+        Runs the HMC alogrithm for N_thermal times
+        """
+        with alive_bar(self.N_thermal) as bar:
+        
+            for i in range(self.N_thermal):
+                self.HMC()
+                bar()
+    
+    def generate_measurements(self, observable):
+        """
+        Runs the HMC N_measurment times and records the observable value in each measurement.
+        """
+
+        results = [0 for i in range(self.N_measurement)]
+        
+        print('Measurements with a = ' + str(self.a) + " N = " +str(self.N))
+        
+        with alive_bar(self.N_measurement) as bar:
+
+
+            for i in range(self.N_measurement):
+
+                for j in range(self.N_sweeps):
+
+                    self.HMC()
+                
+                self.DH[i]  = self.delta_H 
+                
+                results[i] = observable(self.U)
+               
+                bar()
+        
+        print('------------------------------')
+        
+        return results
+
+
+
+
+"""a = Chiral(2,2)
 #print(Chiral.action(a.U,a.beta))
-print(a.action_2(a.U))
+print(a.action(a.U,1))
 #print(Chiral.action(a.U,a.beta))
+"""
+"""N = 100
+generators = np.zeros((8,3,3),dtype=complex)
+generators[0][0,1] = 1
+generators[0][1,0] = 1
+generators[1][0,1] = -1j
+generators[1][1,0] = 1j
+generators[2][0,0]  = 1
+generators[2][1,1] = -1
+generators[3][0,2] = 1
+generators[3][2,0] = 1
+generators[4][0,2] = -1j
+generators[4][2,0] = 1j
+generators[5][1,2] = 1
+generators[5][2,1] = 1
+generators[6][1,2] = -1j
+generators[6][2,1] = 1j
+generators[7][0,0] = 1/np.sqrt(3)
+generators[7][1,1] = 1/np.sqrt(3)
+generators[7][2,2] = -2/np.sqrt(3)
+p_i = np.random.normal(loc=0,scale=1,size=(N,N, 8))
+p = np.zeros((N,N,3,3))
+for i in range(N):
+    for j in range(N):
+        p[i,j] = sum(p_i[i,j,k] * generators[k] for k in range(8))
+"""
