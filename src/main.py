f3e3e10d3f5e4fc88e7f96017ae8fb363597a2b3
Pmorande27 <pmorande27@gmail.com>
2023-11-12 21:08:57 +0000

    fix bug in ww corr

--- src/main.py
+++ src/main.py
@@ -14,13 +14,11 @@ def main():
     N = 64
     SU = 2
     betas1 = [0.8667]
-    betas = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,4.0,4.1,4.2,4.3,4.4,4.5,4.6,4.7,4.8,4.9,5.0,5.1,5.2,5.3,5.4,5.5,5.6,5.7,5.8,5.9,6.0,6.1,6.2,6.3,6.4,6.5,6.6,6.7,6.8,6.9,7.0,7.1,7.2,7.3,7.4,7.5,7.6,7.7,7.8,7.9,8.0,8.1,8.2,8.3,8.4,8.5,8.5,8.6,8.7,8.8,8.9,9.0,9.1,9.2,9.3,9.4,9.5,9.6,9.7,9.8,9.9,10.0,10.1,10.2,10.3,10.4,10.5,10.6,10.7,10.8,10.9,11.0]
     order = 0
-    betas2 = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6,3.7,3.8,3.9,4.0]
     N_order = 0
     N_tau = 10
     N_thermal = 10**3
-    N_measure = 1000
+    N_measure = 10**3
     for beta in betas1:
         #N_tau = Chiral_run.calibration(beta,N,SU,order,N_order,N_tau)
         #Chiral_run.measure_func_1D(beta,N,SU,order,N_order,N_measure,N_thermal,lambda U:Chiral.Measure_ww_corr(U,SU),"ww corr")
@@ -44,8 +42,8 @@ def main():
         return  (np.cosh(1/dE*(t-L/2))-1 ) / (np.cosh(1/dE*L/2) -1)
     a =curve_fit(model,xdata,ydata,absolute_sigma=True,)
     xs = [model(d,a[0][0]) for d in xdata]
-    print(a[0][0])"""
-    """plt.plot(xdata,xs)
+    print(a[0][0])
+    plt.plot(xdata,xs)
 
     plt.plot(xdata,np.load(file_name),label='holaaa')
     plt.legend()
@@ -114,8 +112,10 @@ def Greens_mom_2(beta,N,SU,order,N_order,N_measure,N_thermal):
     for i in range(N):
 
         values[i],values_err[i]= Stats.Stats(data[i]).estimate()
+    
     xdata = np.arange(N)
-    plt.errorbar(xdata,values/values[0],values_err/values[0])
+    print(values)
+    plt.plot(xdata,values/values[0])
     plt.show()
     
     
