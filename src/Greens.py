f4845230ffdda7e62ff84205188028036c45c79d
Pablo Morande <pmorande27@gmail.com>
2023-11-15 15:25:19 +0000

    Add error propagation to estimate the error of the wall to point correlation length

--- src/Greens.py
+++ src/Greens.py
@@ -5,12 +5,15 @@ def Greens_mom(beta,N,SU,order,N_order,N_measure,N_thermal):
     file_name = "ChiralResults/Processed/"+observable_name+"/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.npy'
     values,errors  = np.load(file_name)
     Greens_zero_mom = np.zeros((N+1))
+    error_zero = np.zeros((N+1))
     for i in range(N):
         Greens_zero_mom[i] = np.sum(values[i])
+        error_zero[i] = np.sqrt(np.sum(errors[i]**2))
     Greens_zero_mom[N] = Greens_zero_mom[0]
+    error_zero[N] = error_zero[0]
     observable_name = 'Greens 0 Mom'
     file_name = "ChiralResults/Processed/"+observable_name+"/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.npy'
-    np.save(file_name,Greens_zero_mom/Greens_zero_mom[0])
+    np.save(file_name,(Greens_zero_mom/Greens_zero_mom[0],error_zero/Greens_zero_mom[0]))
 def susceptibility_from_complete_greens_function(beta,N,SU,order,N_order,N_measure,N_thermal):
     observable_name = 'Greens 2'
     file_name = "ChiralResults/Processed"+observable_name+"/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.npy'
@@ -47,6 +50,7 @@ def second_moment_correletion_length(beta,N,SU,order,N_order,N_measure,N_thermal
     #print(np.sum(Gf1))
     Gf0 = np.sum(np.array([[values[i,j] for j in range(N)] for i in range(N)]))
     """
+    print(Gf[0,1]-Gf[0,1].real)
     return (1/(4*np.sin(np.pi/N)*np.sin(np.pi/N))*(Gf[0,0]/Gf[0,1]-1))**0.5,Gf[0,0]
 
 def second_moment_correletion_length_three(beta,N,SU,order,N_order,N_measure,N_thermal):
