bb05e7cb931cdc923556df4638d7f0b3040a7891
Pablo Morande <pmorande27@gmail.com>
2023-11-15 17:36:37 +0000

    added diagonal ww corr function

--- src/plotting.py
+++ src/plotting.py
@@ -302,3 +302,20 @@ def plot_Greens_0_mom(beta,N,SU,order,N_order,N_measure,N_thermal):
     ax.plot(xdata,ys)
     ax.spines[['right', 'top']].set_visible(False)
     plt.show()
+def plot_Greens_diags(beta,N,SU,order,N_order,N_measure,N_thermal):
+    observable_name = 'Greens Diags'
+    file_name = "ChiralResults/Processed/"+observable_name+"/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.npy'
+    values,errors = np.load(file_name)
+    xdata = np.arange(N+1)
+    def model(t, dE):
+        L = N
+        return  (np.cosh(1/dE*(t-L/2)) -1) / (np.cosh(1/dE*L/2)-1)
+    a =curve_fit(model,xdata,values/values[0])
+    ys = [model(d,a[0][0]) for d in xdata]
+    
+    print(a)
+    ax = plt.subplot(111)
+    ax.errorbar(xdata,values,yerr=errors,fmt='.k')
+    ax.plot(xdata,ys)
+    ax.spines[['right', 'top']].set_visible(False)
+    plt.show()
