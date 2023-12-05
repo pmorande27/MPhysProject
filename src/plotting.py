18668b94eb012861c8cf25890388ac8facae3b5e
pmorande27 <pmorande27@gmail.com>
2023-12-05 13:13:27 +0100

    add plots

--- src/plotting.py
+++ src/plotting.py
@@ -5,8 +5,119 @@ import numpy as np
 import matplotlib.pyplot as plt
 from Stats import Stats
 from scipy.optimize import curve_fit
+def mass_plot(beta,N,SU,order,N_order,N_measure,N_thermal,lower,upper):
+    observable_name = 'ww corr'
+    file_name = "ChiralResults/Processed/"+observable_name+"/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.npy'
+    ww_cor, ww_cor_err = np.load(file_name)
+    ww_cor, ww_cor_err = np.load(file_name)
+    ww_cor, ww_cor_err = ww_cor/ww_cor[0], ww_cor_err/ww_cor[0]
+    xdata = np.arange(N+1)
 
-def Greens_mom_2(beta,N,SU,order,N_order,N_measure,N_thermal):
+    N_2 = int(N/2) 
+    ds = np.arange(N_2+1) # wall separations covering half the lattice length
+    cor = 1/2 * (ww_cor[:N_2+1] + ww_cor[N_2:][::-1])
+    cor_err = np.sqrt(ww_cor_err[:N_2+1]**2 + ww_cor_err[N_2::-1]**2) / np.sqrt(2)
+    rel_err = cor_err / cor # relative error
+    cor_1, cor_err_1 = np.roll(cor, -1), np.roll(cor_err, -1) # shift to d+1
+    rel_err_1 = cor_err_1 / cor_1
+    cor__1, cor_err__1 = np.roll(cor, 1), np.roll(cor_err, 1) # shift to d-1
+    rel_err__1 = cor_err__1 / cor__1
+
+    A, B = cor_1/cor, cor__1/cor
+    x = (A+B)/2 
+    m_eff = np.arccosh(x)
+
+    delta_x = 1/2 * (A*(rel_err_1 - rel_err) + B*(rel_err__1 - rel_err))
+    # delta_x = A/2*(np.sqrt(rel_err_1**2 + rel_err**2)) + B/2*(np.sqrt(rel_err__1**2 + rel_err**2))
+    m_eff_err = 1/np.sqrt(x**2-1) * delta_x
+
+    cor_1 = np.roll(cor, -1) # shift to d+1
+    #m_eff = - np.log(cor_1 / cor)
+    #m_eff_err = np.roll(cor_err, -1)/cor_1 - cor_err/cor 
+    #plt.errorbar(ds, m_eff, yerr=m_eff_err, fmt='.', capsize=2)
+    def model(x, m):
+        return m
+    one = ds <= upper
+    two = ds >= lower
+
+    mask = one & two
+    popt, pcov = curve_fit(model, ds[mask], m_eff[mask], sigma=m_eff_err[mask], absolute_sigma=True)
+    print(popt, np.sqrt(pcov[0][0]))
+    cor_len = 1/popt[0]
+    cor_len_err = np.sqrt(pcov[0][0])/popt[0]**2
+    ys = np.array([popt[0] for x in ds[mask]])
+    r = m_eff[mask] - ys
+    reduced_chi2 = np.sum((r/m_eff_err[mask])**2) / (mask.size -1)
+    print(reduced_chi2)
+    plt.errorbar(ds, m_eff, yerr=m_eff_err, fmt='.', capsize=2,label='$\\xi = %.3f \pm %.3f$\n $\chi^2/DoF = %.3f$'%(cor_len, cor_len_err, reduced_chi2))
+    plt.legend()
+    plt.plot(ds[mask], ys, c='g', label='$m = %.3f \pm %.3f$'%(popt[0], np.sqrt(pcov[0][0])))
+    #plt.plot(ds, model(ds, *popt), c='g', label='$m = %.3f \pm %.3f$'%(popt[0], np.sqrt(pcov[0][0])))
+    plt.ylim(popt[0]-0.2, popt[0]+0.2)
+    plt.title( "beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal),fontsize = 5)
+    plt.ylabel('m')
+    plt.xlabel('t')
+    plt.xlim(0,21+1)
+    file_name = "ChiralResults/Processed/Plots/Mass Plot"+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.svg'
+
+    plt.savefig(file_name)
+    plt.show()
+def correlation_length_automatic_fit(beta,N,SU,order,N_order,N_measure,N_thermal,lower):
+    observable_name = 'ww corr'
+    file_name = "ChiralResults/Processed/"+observable_name+"/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.npy'
+    ww_cor, ww_cor_err = np.load(file_name)
+    ww_cor, ww_cor_err = ww_cor/ww_cor[0], np.sqrt((ww_cor_err/ww_cor[0])**2 + (ww_cor/ww_cor[0]**2 * ww_cor_err[0])**2)
+    xdata = np.arange(N+1)
+
+    N_2 = int(N/2) 
+    ds = np.arange(N_2+1) # wall separations covering half the lattice length
+    cor = 1/2 * (ww_cor[:N_2+1] + ww_cor[N_2:][::-1])
+    cor_err = np.sqrt(ww_cor_err[:N_2+1]**2 + ww_cor_err[N_2::-1]**2) / np.sqrt(2)
+
+    # store processed correlation function data
+    def model(d,xi):
+        return (np.cosh((d-N_2)/xi)-1 ) / (np.cosh(N_2/xi)-1 )
+    cor_length,cor_length_err,reduced_chi2 = np.zeros(N_2),np.zeros(N_2),np.zeros(N_2)
+    for i,upper_fit in enumerate(range(lower,N_2)):
+    # perform the fit  
+        one = ds <= upper_fit
+        two = ds >=lower
+        mask = one*two# fitting range
+        popt, pcov = curve_fit(model, ds[mask], cor[mask], sigma=cor_err[mask], absolute_sigma=True)
+        cor_length[i] = popt[0] # in units of lattice spacing
+        cor_length_err[i] = np.sqrt(pcov[0][0])
+
+        r = cor[mask] - model(ds[mask], *popt)
+        reduced_chi2[i] = np.sum((r/cor_err[mask])**2) / (mask.size - 1) # dof = number of observations - number of fitted parameters
+    i = np.argmin(abs(reduced_chi2-[1 for i in range(N_2)]))
+    #print(range(1,N_2+1)[i])
+    one = ds <= range(1,N_2+1)[i]
+    two = ds >= lower
+    mask = one*two# fitting range
+    #print(mask)
+    #mask = ds <= upper_fit
+    reduced_chi2 = reduced_chi2[i]
+    cor_length = cor_length[i]
+    cor_length_err = cor_length_err[i]
+    
+    fig = plt.figure(figsize=(8,6))
+    print(ds[mask])
+    plt.errorbar(ds, cor, yerr=cor_err, fmt='.', capsize=2)
+    cor_length,cor_err = cor_length,cor_err
+    ds_fit = np.linspace(ds[mask][0], ds[mask][-1], 500)
+    plt.plot(ds_fit, model(ds_fit,*popt), c='g', label='$\\xi = %.3f \pm %.3f$\n $\chi^2/DoF = %.3f$'%(cor_length, cor_length_err, reduced_chi2))
+    plt.yscale('log')
+    plt.ylim(10**(-4),10**0)
+    plt.xlabel(r'wall separation $d$ [$a$]')
+    plt.title( "beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal),fontsize = 5)
+    plt.ylabel('wall wall correlation $C_{ww}(d)$')
+    plt.legend(prop={'size':12}, frameon=True, loc='upper right')
+    file_name = "ChiralResults/Processed/Plots/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.svg'
+
+    plt.savefig(file_name)
+    plt.show()
+
+def correlation_length_manual_fit(beta,N,SU,order,N_order,N_measure,N_thermal,lower,upper):
     observable_name = 'ww corr'
     file_name = "ChiralResults/Processed/"+observable_name+"/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.npy'
     ww_cor, ww_cor_err = np.load(file_name)
@@ -20,29 +131,33 @@ def Greens_mom_2(beta,N,SU,order,N_order,N_measure,N_thermal):
 
     # store processed correlation function data
     def model(d,xi):
-        return (np.cosh((d-N_2)/xi) - 1) / (np.cosh(N_2/xi) - 1)
-
-    # perform the fit  
-    mask = cor > 0 # fitting range
-    popt, pcov = curve_fit(model, ds[mask], cor[mask], sigma=cor_err[mask], absolute_sigma=True)
-    cor_length = popt[0] # in units of lattice spacing
+        return (np.cosh((d-N_2)/xi)) / (np.cosh(N_2/xi))
+    upper_fit = upper
+    lower_fit = lower
+    one = ds <= upper_fit
+    two = ds >=lower_fit
+    mask = one*two# fitting range
+    popt, pcov = curve_fit(model, ds[mask], cor[mask], sigma=cor_err[mask], absolute_sigma=True, bounds=([0],[np.inf]))
+    cor_length= popt[0] # in units of lattice spacing
     cor_length_err = np.sqrt(pcov[0][0])
 
     r = cor[mask] - model(ds[mask], *popt)
     reduced_chi2 = np.sum((r/cor_err[mask])**2) / (mask.size - 1) # dof = number of observations - number of fitted parameters
-
+    
     
     fig = plt.figure(figsize=(8,6))
 
     plt.errorbar(ds, cor, yerr=cor_err, fmt='.', capsize=2)
     cor_length,cor_err = cor_length,cor_err
-    ds_fit = np.linspace(0, ds[mask][-1], 500)
+    ds_fit = np.linspace(ds[mask][0], ds[mask][-1], 500)
     plt.plot(ds_fit, model(ds_fit,*popt), c='g', label='$\\xi = %.3f \pm %.3f$\n $\chi^2/DoF = %.3f$'%(cor_length, cor_length_err, reduced_chi2))
     plt.yscale('log')
+    plt.ylim(10**(-4),10**0)
     plt.xlabel(r'wall separation $d$ [$a$]')
+    plt.title( "beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal),fontsize = 5)
     plt.ylabel('wall wall correlation $C_{ww}(d)$')
     plt.legend(prop={'size':12}, frameon=True, loc='upper right')
-    file_name = "ChiralResults/Processed/Plots/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.svg'
+    file_name = "ChiralResults/Processed/Plots/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+' manual.svg'
 
     plt.savefig(file_name)
     plt.show()
@@ -337,7 +452,7 @@ def plot_Greens_0_mom(beta,N,SU,order,N_order,N_measure,N_thermal):
     plt.legend(prop={'size':12}, frameon=True, loc='upper right')
     plt.show()
 def plot_Greens_diags(beta,N,SU,order,N_order,N_measure,N_thermal):
-    observable_name = 'Greens Diags'
+    observable_name = 'Greens diags'
     file_name = "ChiralResults/Processed/"+observable_name+"/"+observable_name+" beta = " + str(beta) + " N = " + str(N)  + " SU = " + str(SU)+" Order = "  + str(order)+" N Order = "  + str(N_order)+" N measurements = "  + str(N_measure)+" N Thermal = "  + str(N_thermal)+'.npy'
     values,errors = np.load(file_name)
     ww_cor,ww_cor_err = np.load(file_name)
@@ -366,7 +481,7 @@ def plot_Greens_diags(beta,N,SU,order,N_order,N_measure,N_thermal):
     fig = plt.figure(figsize=(8,6))
 
     plt.errorbar(ds, cor, yerr=cor_err, fmt='.', capsize=2)
-    cor_length,cor_err = cor_length/np.sqrt(2),cor_err/np.sqrt(2)
+    cor_length,cor_length_err = cor_length/np.sqrt(2),cor_length_err/np.sqrt(2)
     ds_fit = np.linspace(0, ds[mask][-1], 500)
     plt.plot(ds_fit, model(ds_fit,*popt), c='g', label='$\\xi = %.3f \pm %.3f$\n $\chi^2/DoF = %.3f$'%(cor_length, cor_length_err, reduced_chi2))
     plt.yscale('log')
